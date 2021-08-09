eea0312350cfb5dfcf4e06ac83c143c52a225c8a
skanehira <sho19921005@gmail.com>
2021-08-09 22:31:17 +0900

    add plugin key map

--- denops/germanium/main.ts
+++ denops/germanium/main.ts
@@ -5,6 +5,9 @@ export async function main(denops: Denops): Promise<void> {
     `command! -complete=file -nargs=* -range Germanium call denops#notify("${denops.name}", "germanium", [<line1>, <line2>, <f-args>])`,
   );
 
+  await denops.cmd(`nnoremap <silent> <Plug>(Germanium) :<C-u>Germanium<CR>`);
+  await denops.cmd(`xnoremap <silent> <Plug>(Germanium) :Germanium<CR>`);
+
   denops.dispatcher = {
     async germanium(start: unknown, end: unknown, arg: unknown): Promise<void> {
       if (!isNumber(start) || !isNumber(end)) {
