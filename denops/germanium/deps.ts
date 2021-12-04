072e752ec829213946fbac1675eb840c0ba4c9b7
skanehira <sho19921005@gmail.com>
2021-12-04 11:15:25 +0900

    deps: update modules

--- denops/germanium/deps.ts
+++ denops/germanium/deps.ts
@@ -1,10 +1,10 @@
-export * from "https://deno.land/x/denops_std@v1.0.1/mod.ts";
-export * from "https://deno.land/x/denops_std@v1.0.1/test/mod.ts";
-export * from "https://deno.land/x/denops_std@v1.0.1/variable/mod.ts";
-export * from "https://deno.land/x/unknownutil@v1.1.0/mod.ts";
-export * from "https://deno.land/std@0.103.0/testing/asserts.ts";
-export * as path from "https://deno.land/std@0.103.0/path/mod.ts";
-export * as io from "https://deno.land/std@0.103.0/io/mod.ts";
+export * from "https://deno.land/x/denops_std@v2.2.0/mod.ts";
+export * from "https://deno.land/x/denops_std@v2.2.0/test/mod.ts";
+export * from "https://deno.land/x/denops_std@v2.2.0/variable/mod.ts";
+export * from "https://deno.land/x/unknownutil@v1.1.4/mod.ts";
+export * from "https://deno.land/std@0.117.0/testing/asserts.ts";
+export * as path from "https://deno.land/std@0.117.0/path/mod.ts";
+export * as io from "https://deno.land/std@0.117.0/io/mod.ts";
 export * from "./types.ts";
 export * from "./builder.ts";
 export * from "./run.ts";
